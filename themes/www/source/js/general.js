b90c8a51606bc6ac217e241901bebd0c37d121be
David B <odga95@gmail.com>
2022-09-14 17:00:29 -0500

    header

--- themes/www/source/js/general.js
+++ themes/www/source/js/general.js
@@ -1,3 +1,29 @@
+const menuIcon = document.querySelector('.menu');
+
 document.addEventListener("DOMContentLoaded", function () {
 
+    menuIcon.addEventListener('click', menuOpen);
+    window.addEventListener("scroll", function () {
+        var head = document.querySelector(".navbar")
+        head.classList.toggle("navbar-fixed-top", window.scrollY > 0);
+
+    })
 });
+
+function menuOpen() {
+
+    const display = document.querySelector('.navbar-nav').style.display;
+    if (!display || display === 0) {
+        document.querySelector('.navbar-nav').style.display = 'block'
+        setTimeout(() => {
+            document.querySelector('.navbar-nav').style.opacity = '1'
+        }, 2);
+    }
+    else {
+        document.querySelector('.navbar-nav').style.opacity = ''
+        setTimeout(() => {
+            document.querySelector('.navbar-nav').style.display = ''
+        }, 1);
+    }
+}
+
