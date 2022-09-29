e053fb995d8578cdf8d697f5e2083c262cb6d391
David B <odga95@gmail.com>
2022-09-29 17:04:46 -0500

    Blog

---
+++ source/blog/index.md
@@ -0,0 +1,6 @@
+---
+id: blog
+title: blog
+date: 2022-09-13 19:42:27
+layout: blog
+---
--- themes/www/source/js/general.js
+++ themes/www/source/js/general.js
@@ -1,8 +1,10 @@
 const menuIcon = document.querySelector('.menu');
+const menuIcon2 = document.querySelector('.icon');
 
 document.addEventListener("DOMContentLoaded", function () {
 
     menuIcon.addEventListener('click', menuOpen);
+    menuIcon2.addEventListener('click', subMenuOpen);
     window.addEventListener("scroll", function () {
         var head = document.querySelector(".navbar")
         head.classList.toggle("navbar-fixed-top", window.scrollY > 0);
@@ -24,6 +26,29 @@ function menuOpen() {
         setTimeout(() => {
             document.querySelector('.navbar-nav').style.display = ''
         }, 1);
+    }
+}
+function subMenuOpen() {
+
+    const display = document.querySelector('.subMenu').style.display;
+    if (!display || display === 0) {
+        document.querySelector('.subMenu').style.display = 'block'
+        setTimeout(() => {
+            document.querySelector('.subMenu').style.opacity = '1'
+            document.querySelector('.fa-angle-right').style.transform = "rotate(90deg)";
+            document.querySelector('.fa-angle-right').style.color = "#0f67f6";
+
+        }, 2);
+    }
+    else {
+        document.querySelector('.subMenu').style.opacity = ''
+        setTimeout(() => {
+            document.querySelector('.subMenu').style.display = ''
+            document.querySelector('.fa-angle-right').style.transform = "rotate(0deg)";
+            document.querySelector('.fa-angle-right').style.color = "#6b6b6b";
+        }, 1);
+
+        
     }
 }
 
